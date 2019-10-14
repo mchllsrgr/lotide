1c3178c0bf5821849d3436c4d217646285de79c4
Michelle Siregar <michellesiregar@gmail.com>
2019-10-14 23:12:49 +0000

    initial commit of head - check if first item in array parameter is equivalent to second paramenter value

--- head.js
+++ head.js
@@ -12,7 +12,7 @@ function head(array) {
   return array[0];
 }
 
-//test code
+// test code
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([6]), 5);
