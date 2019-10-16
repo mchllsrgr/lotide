1a4d7fadfd92269f31e4963517058c878d3ab8ec
Michelle Siregar <michellesiregar@gmail.com>
2019-10-16 21:15:21 +0000

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// check if two inputs are equivalent
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(string) {
+  const results = {};
+  const noSpaces = string.split(" ").join("");
+  for (const letter of noSpaces) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  console.log(results);
+  return results;
+}
+
+countLetters('lighthouse in the house')
