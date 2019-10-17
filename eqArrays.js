aeb40e11acac354ffbb2f8e9bda09ae312069635
Michelle Siregar <michellesiregar@gmail.com>
2019-10-17 01:09:09 +0000

    added length conditional

--- eqArrays.js
+++ eqArrays.js
@@ -9,12 +9,17 @@ const assertEqual = function(actual, expected) {
 
 // check if two arrays are equal
 const eqArrays = function(arr1, arr2) {
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  if (arr1.length === arr2.length) {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } else {
+        return true;
+      }
     }
+  } else {
+    return false;
   }
-  return true;
 }
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
