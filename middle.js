112149118cef387a07afe0405497548b52ba28aa
Michelle Siregar <michellesiregar@gmail.com>
2019-10-21 19:41:47 +0000

    refactored middle, created middleTest

--- middle.js
+++ middle.js
@@ -1,22 +1,8 @@
 // check if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 // take two arrays and console.log appropriate message
-const assertArraysEqual = function(arr1, arr2) {
-  let arrayCheck = eqArrays(arr1, arr2);
-  if (arrayCheck === true) {
-    console.log(`✅ Assertion passed: ${arr1} === ${arr2}`);
-  } else if (arrayCheck === false) {
-    console.log(`🛑 Assertion failed: ${arr1} !== ${arr2}`);
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
 // take the middle element(s) of an array
 const middle = function(array) {
@@ -32,9 +18,5 @@ const middle = function(array) {
   return middleNum;
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([, 2, 3, 4, 5, 6]), [3, 4]);
+
+module.exports = middle;
